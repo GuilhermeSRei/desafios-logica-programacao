def190ecded810b71166d26f840c9c34e3ec1ccb
GuilhermeSRei <139374621+GuilhermeSRei@users.noreply.github.com>
2024-03-15 18:58:39 -0300

    atualização

---
+++ desafio1/desafio1.js
@@ -0,0 +1,10 @@
+let listaGenerira = [];
+
+let linguagensDeProgamação = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+
+
+linguagensDeProgamação.push('Java', 'Ruby', 'Golang')
+
+
+console.log(linguagensDeProgamação[2])
